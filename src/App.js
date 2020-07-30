a21b33edfce42865623a79bfef4f125ffa0e5e6b
coseo12 <momo_12@naver.com>
2020-07-30 17:44:11 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <>
-      <h1>To Dos</h1>
+      <h1>state {state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>increment</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>decrement</button>
     </>
   );
 }
