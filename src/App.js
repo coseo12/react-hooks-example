f7e9442bd6667a69aff633bf399bb294cbaa5088
coseo12 <momo_12@naver.com>
2020-07-30 15:36:36 +0900

    todo init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Lang from './context';
-import Screen from './Screen';
-import translations from './translations';
 
 function App() {
   return (
-    <Lang defaultLang="en" translations={translations}>
-      <Screen />
-    </Lang>
+    <>
+      <h1>To Dos</h1>
+    </>
   );
 }
 
